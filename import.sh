3a5f6684ac1b21a7248e9b6903d36e59df8beb6f
someone2639 <someone2639@gmail.com>
2024-05-20 23:45:28 -0400

    update rt64, recomp runtime

--- import.sh
+++ import.sh
@@ -5,6 +5,6 @@ rm -rf RecompiledFuncs
 cp -R ../N64Recomp/RecompiledFuncs .
 cp -R ../N64Recomp/rsp/ .
 cp FFFFFFFF.c RecompiledFuncs/
-cp dma_overlay_load.c RecompiledFuncs/
-cp fatal_printf.c RecompiledFuncs/
+# cp dma_overlay_load.c RecompiledFuncs/
+# cp fatal_printf.c RecompiledFuncs/
 touch src/recomp/overlays.cpp
