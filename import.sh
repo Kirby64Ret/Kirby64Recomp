ee243f83b5a9798c47711b91a337a3dce78ac40b
someone2639 <someone2639@gmail.com>
2024-10-26 01:40:03 -0400

    Game actually does stuff

--- import.sh
+++ import.sh
@@ -4,7 +4,7 @@ set -v
 rm -rf RecompiledFuncs
 cp -R ../N64Recomp/RecompiledFuncs .
 cp -R ../N64Recomp/rsp/ .
-cp FFFFFFFF.c RecompiledFuncs/
-cp dma_overlay_load.c RecompiledFuncs/
-cp fatal_printf.c RecompiledFuncs/
-touch src/recomp/overlays.cpp
+# cp FFFFFFFF.c RecompiledFuncs/
+# cp dma_overlay_load.c RecompiledFuncs/
+# cp fatal_printf.c RecompiledFuncs/
+# touch src/recomp/overlays.cpp
