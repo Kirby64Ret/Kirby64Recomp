ebea47ffeb5409a80759e0c2e72d71e68c7769e8
someone2639 <someone2639@gmail.com>
2024-10-27 19:14:37 -0400

    remove extraneous files

--- FFFFFFFF.c
+++
@@ -1,8 +0,0 @@
-#include "recomp.h"
-#include "disable_warnings.h"
-
-void _FFFFFFFF(uint8_t* rdram, recomp_context* ctx) {
-    printf("AMBIGUOUS FUNCTION DETECTED");
-    assert(false);
-    return;
-}
--- fatal_printf.c
+++
@@ -1,8 +0,0 @@
-#include "recomp.h"
-#include "disable_warnings.h"
-
-void fatal_printf(uint8_t* rdram, recomp_context* ctx) {
-    printf((const char*)ctx->r4);
-    assert(false);
-    return;
-}
